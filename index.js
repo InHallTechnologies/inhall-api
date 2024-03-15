1cd00eaa1d5b8fe524ed0de9073823caecb45bee
InHall Technologies <inhall.updatebackup@gmail.com>
2024-03-15 17:09:58 +0530

    Pre final

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.use(cors());
 app.use(express.json());
 app.use("/blogs", blogRouter);
 
+app.get('/', (req, res) => {
+    res.send("WORKING")
+})
 
 app.listen(PORT, () => {
     console.log("Server started at port", PORT);
